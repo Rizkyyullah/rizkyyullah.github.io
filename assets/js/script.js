4c40401c5717617bdfb4d8c020b3f9edc9ef466f
rizkyyullah <rizkyyullah1945@gmail.com>
2023-07-05 23:03:44 +0700

    Membuat Hero Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const li = document.querySelectorAll('header ul li');
 
+// Menampilkan garis bawah saat a header di hover
 li.forEach(li =>
   li.addEventListener("click", () => {
       const arr = Array.from(li.parentElement.children);
@@ -7,3 +8,19 @@ li.forEach(li =>
       li.classList.add("active");
   })
 );
+
+// Sticky Navbar
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+// Menu Toggle/Hamburger Menu
+const menuToggle = document.querySelector('.menu-toggle');
+const header = document.querySelector('header');
+
+menuToggle.onclick = () => {
+  header.classList.toggle('show');
+};
+
+// Tes
