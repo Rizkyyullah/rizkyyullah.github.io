7e361e7b6c341317268ca50899d56d453035a414
rizkyyullah <rizkyyullah1945@gmail.com>
2025-08-04 10:53:42 +0700

    create navigation and mobile responsive

--- public/assets/script/index.js
+++ public/assets/script/index.js
@@ -1,9 +1,25 @@
-window.addEventListener("load", () => {
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
+const links = document.querySelectorAll('#menu a');
+
+window.addEventListener('load', () => {
   setTimeout(() => {
-    const loader = document.querySelector("#loader");
-    loader.style.opacity = "0";
+    const loader = document.querySelector('#loader');
+    loader.style.opacity = '0';
     setTimeout(() => {
-      loader.style.display = "none";
+      loader.style.display = 'none';
     }, 500);
   }, 2_000);
+});
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menu.classList.remove('active');
+  });
 });
